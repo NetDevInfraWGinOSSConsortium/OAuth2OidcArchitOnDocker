c6638d45c9b36b737b9750d53c0dac00f3229258
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2020-08-04 14:07:03 +0900

    Initialized.

---
+++ DockerHub/Push/MultiPurposeAuthSiteCore/0_Transform4DockerBuild.bat
@@ -0,0 +1,6 @@
+xcopy /Y /E "..\..\programs\MultiPurposeAuthSiteCore\MultiPurposeAuthSiteCore" "MultiPurposeAuthSiteCore\"
+xcopy /Y /E "..\..\files" "MultiPurposeAuthSiteCore\files\"
+xcopy /Y /E "..\..\programs\OpenTouryoAssemblies\Build_netcore30" "MultiPurposeAuthSiteCore\Assemblies\"
+xcopy /Y /E "..\..\programs\CommonLibrary\bin\netcore\Debug" "MultiPurposeAuthSiteCore\Assemblies\"
+copy /Y ".\MVC_Sample.csproj" "MVC_Sample\MVC_Sample.csproj"
+del /Q "MVC_Sample\Dockerfile"
---
+++ DockerHub/Push/MultiPurposeAuthSiteCore/3_Push2DockerHub.bat
@@ -0,0 +1,22 @@
+docker images
+
+pause
+
+set IMAGE_ID_STR=
+set /p IMAGE_ID_STR="対象(multipurposeauthsitecore:latest)のイメージIDを入力: "
+echo 入力したイメージID: %IMAGE_ID_STR%
+
+pause
+
+docker tag %IMAGE_ID_STR% osscjpdevinfra/multipurposeauthsitecore:OAuth2OidcArchitOnDocker
+docker images
+
+pause
+
+docker login
+
+pause
+
+docker push osscjpdevinfra/multipurposeauthsitecore:OAuth2OidcArchitOnDocker
+
+pause
